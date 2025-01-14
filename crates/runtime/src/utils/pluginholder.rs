3a12114cb496c6b59a0cef528117e68c02285216
Sanandan Sashikumar <cheesycod@outlook.com>
2025-01-14 17:22:41 +0000

    fix compile

--- crates/runtime/src/utils/pluginholder.rs
+++ crates/runtime/src/utils/pluginholder.rs
@@ -42,7 +42,7 @@ impl<T: KhronosContext> PluginSet<T> {
         //self.add_plugin("@antiraid/page", antiraid::page::init_plugin); [will be implemented later on, once we know how templates are being used]
         self.add_plugin("@antiraid/permissions", antiraid::permissions::init_plugin);
         self.add_plugin("@antiraid/promise", antiraid::promise::init_plugin);
-        self.add_plugin("@antiraid/stings", antiraid::stings::init_plugin);
+        self.add_plugin("@antiraid/stings", antiraid::stings::init_plugin::<T>);
         self.add_plugin("@antiraid/datetime", antiraid::datetime::init_plugin);
         self.add_plugin("@antiraid/typesext", antiraid::typesext::init_plugin);
         self.add_plugin("@antiraid/userinfo", antiraid::userinfo::init_plugin::<T>);
