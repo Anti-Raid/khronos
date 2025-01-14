4672a10f46bfdc241545249d95fadbff40ba9ec5
Sanandan Sashikumar <cheesycod@outlook.com>
2025-01-14 03:33:00 +0000

    fix

--- crates/runtime/src/traits/sample.rs
+++ crates/runtime/src/traits/sample.rs
@@ -1,6 +1,7 @@
 use crate::utils::executorscope::ExecutorScope;
 
 use super::context::KhronosContext;
+use super::discordprovider::DiscordProvider;
 use super::kvprovider::{self, KVProvider};
 
 #[derive(Clone)]
@@ -11,6 +12,7 @@ pub struct SampleKhronosContext {
 impl KhronosContext for SampleKhronosContext {
     type Data = ();
     type KVProvider = SampleKVProvider;
+    type DiscordProvider = SampleDiscordProvider;
 
     fn data(&self) -> Self::Data {
         todo!()
@@ -32,8 +34,12 @@ impl KhronosContext for SampleKhronosContext {
         None::<serenity::all::CurrentUser>
     }
 
-    fn kv_executor(&self, _scope: ExecutorScope) -> Option<Self::KVProvider> {
-        None
+    fn kv_provider(&self, _scope: ExecutorScope) -> Option<Self::KVProvider> {
+        Some(SampleKVProvider {})
+    }
+
+    fn discord_provider(&self, _scope: ExecutorScope) -> Option<Self::DiscordProvider> {
+        Some(SampleDiscordProvider {})
     }
 }
 
@@ -66,6 +72,84 @@ impl KVProvider for SampleKVProvider {
     }
 
     async fn keys(&self) -> Result<Vec<String>, crate::Error> {
+        todo!()
+    }
+}
+
+#[derive(Clone)]
+pub struct SampleDiscordProvider {}
+
+impl DiscordProvider for SampleDiscordProvider {
+    fn attempt_action(&self, _bucket: &str) -> serenity::Result<(), crate::Error> {
+        todo!()
+    }
+
+    async fn guild(
+        &self,
+    ) -> serenity::Result<serenity::model::prelude::PartialGuild, crate::Error> {
+        todo!()
+    }
+
+    async fn member(
+        &self,
+        _user_id: serenity::all::UserId,
+    ) -> serenity::Result<Option<serenity::all::Member>, crate::Error> {
+        todo!()
+    }
+
+    async fn guild_channel(
+        &self,
+        _channel_id: serenity::all::ChannelId,
+    ) -> serenity::Result<serenity::all::GuildChannel, crate::Error> {
+        todo!()
+    }
+
+    async fn get_audit_logs(
+        &self,
+        _action_type: Option<serenity::all::audit_log::Action>,
+        _user_id: Option<serenity::model::prelude::UserId>,
+        _before: Option<serenity::model::prelude::AuditLogEntryId>,
+        _limit: Option<serenity::nonmax::NonMaxU8>,
+    ) -> serenity::Result<serenity::model::prelude::AuditLogs, crate::Error> {
+        todo!()
+    }
+
+    async fn get_automod_rules(
+        &self,
+    ) -> serenity::Result<Vec<serenity::model::guild::automod::Rule>> {
+        todo!()
+    }
+
+    async fn get_automod_rule(
+        &self,
+        _rule_id: serenity::all::RuleId,
+    ) -> serenity::Result<serenity::model::guild::automod::Rule> {
+        todo!()
+    }
+
+    async fn edit_channel(
+        &self,
+        _channel_id: serenity::all::ChannelId,
+        _map: impl serde::Serialize,
+        _audit_log_reason: Option<&str>,
+    ) -> serenity::Result<serenity::model::channel::Channel> {
+        todo!()
+    }
+
+    async fn delete_channel(
+        &self,
+        _channel_id: serenity::all::ChannelId,
+        _audit_log_reason: Option<&str>,
+    ) -> serenity::Result<()> {
+        todo!()
+    }
+
+    async fn create_member_ban(
+        &self,
+        _user_id: serenity::all::UserId,
+        _delete_message_seconds: u32,
+        _reason: Option<&str>,
+    ) -> serenity::Result<()> {
         todo!()
     }
 }
