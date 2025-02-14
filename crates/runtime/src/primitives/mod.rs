fe4a672a3cb16cf8563a5a41e0bc37b075605006
Sanandan Sashikumar <cheesycod@outlook.com>
2025-02-14 03:56:42 +0000

    fix

--- crates/runtime/src/primitives/mod.rs
+++ crates/runtime/src/primitives/mod.rs
@@ -12,17 +12,20 @@ pub fn create_userdata_iterator_with_fields<const N: usize>(
 ) -> LuaResult<mlua::Function> {
     let i = RefCell::new(0);
     lua.create_function(move |lua, _: ()| {
-        *i.borrow_mut() += 1;
-        let mut i_val = *i.borrow();
-        if i_val <= fields.len() {
+        let mut i_val = i.borrow_mut();
+        *i_val += 1;
+        if *i_val <= fields.len() {
             loop {
-                let k = fields[i_val - 1];
+                if *i_val > fields.len() {
+                    return (mlua::Value::Nil).into_lua_multi(lua);
+                }
+
+                let k = fields[*i_val - 1];
 
                 let v = match ud.get::<LuaValue>(k.to_string()) {
                     Ok(v) => v,
                     Err(_) => {
-                        *i.borrow_mut() += 1;
-                        i_val += 1;
+                        *i_val += 1;
                         continue;
                     }
                 };
