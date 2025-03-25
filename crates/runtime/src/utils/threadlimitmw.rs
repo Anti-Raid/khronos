82140394b1ee94d5c01b405dc7a2cbf55b5d81d4
Your Name <you@example.com>
2025-03-24 21:22:40 -0400

    replace refcell with cell here

--- crates/runtime/src/utils/threadlimitmw.rs
+++ crates/runtime/src/utils/threadlimitmw.rs
@@ -1,20 +1,20 @@
 pub struct ThreadLimiter {
     thread_limit: usize,
-    threads: std::cell::RefCell<usize>,
+    threads: std::cell::Cell<usize>,
 }
 
 impl ThreadLimiter {
     pub fn new(thread_limit: usize) -> Self {
         Self {
             thread_limit,
-            threads: std::cell::RefCell::new(0),
+            threads: std::cell::Cell::new(0),
         }
     }
 
     /// Returns the current number of threads recorded
     /// by the thread limiter.
     pub fn current_threads(&self) -> usize {
-        *self.threads.borrow()
+        self.threads.get()
     }
 
     /// Returns the thread limit set for this thread limiter.
@@ -23,12 +23,12 @@ impl ThreadLimiter {
     }
 
     fn on_thread_add_impl(&self) -> mlua::Result<()> {
-        let mut threads = self.threads.borrow_mut();
-        if *threads >= self.thread_limit {
+        let threads = self.threads.get();
+        if threads >= self.thread_limit {
             return Err(mlua::Error::external("Thread limit reached"));
         }
 
-        *threads += 1;
+        self.threads.set(threads + 1);
 
         Ok(())
     }
