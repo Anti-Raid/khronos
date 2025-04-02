09b1363f4882a6971b2e6317bda71672738f6fae
Your Name <you@example.com>
2025-04-01 21:59:18 -0400

    fix

--- crates/runtime/src/rt/runtime.rs
+++ crates/runtime/src/rt/runtime.rs
@@ -111,8 +111,6 @@ impl KhronosRuntime {
 
         let scheduler_lib = mlua_scheduler::userdata::scheduler_lib(&lua)?;
 
-        mlua_scheduler::userdata::patch_coroutine_lib(&lua)?;
-
         // Add in basic globals
         if !opts.disable_scheduler_lib {
             lua.globals().set("scheduler", scheduler_lib.clone())?;
