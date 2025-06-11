3764d9c5f6873fa2ba73caa86149b7369a278eb5
Your Name <you@example.com>
2025-06-11 06:11:13 -0400

    add log

--- crates/runtime/src/plugins/antiraid/discord/mod.rs
+++ crates/runtime/src/plugins/antiraid/discord/mod.rs
@@ -236,6 +236,8 @@ impl<T: KhronosContext> LuaUserData for DiscordActionExecutor<T> {
             async move |lua, this, data: LuaValue| {
                 let data =
                     lua.from_value::<structs::AntiRaidCheckPermissionsAndHierarchyOptions>(data)?;
+                
+                println!("user_id={user_id}", user_id=data.user_id);
                 let (partial_guild, member, permissions) = this
                     .check_permissions_and_hierarchy(
                         data.user_id,
