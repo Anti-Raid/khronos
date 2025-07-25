63e6bd4a564f0dccadfea178517f1c0545a24d49
Sanandan Sashikumar <sashiks@rose-hulman.edu>
2025-07-25 18:16:55 +0530

    fix

--- crates/runtime/src/primitives/blob.rs
+++ crates/runtime/src/primitives/blob.rs
@@ -6,8 +6,16 @@
 //! When a Blob is passed into a DataStore/Rust, its contents may be moved
 //! to Rust leaving a empty Blob. `clone` can be used to avoid this. When this
 //! will happen is undefined
+//! 
+//! Blob is also a way to encrypt/decrypt data with AES-256-GCM (using Argon2id for key derivation)
 
 use mluau::prelude::*;
+use rand::{Rng, RngCore};
+use argon2::Argon2;
+use aes_gcm::aead::Aead;
+use aes_gcm::KeyInit;
+use aes_gcm::{Aes256Gcm, Nonce};
+use zeroize::Zeroize;
 
 pub struct Blob {
     /// The data of the blob
@@ -31,6 +39,35 @@ impl FromLua for BlobTaker {
                 message: None
             })
         }
+    }
+}
+
+impl Blob {
+    fn create_aes256_cipher(key: String, salt: &[u8]) -> LuaResult<Aes256Gcm> {
+        let argon2 = Argon2::new(
+            argon2::Algorithm::Argon2id,
+            argon2::Version::V0x13,
+            match argon2::ParamsBuilder::new()
+                .t_cost(1)
+                .m_cost(64 * 1024)
+                .p_cost(4)
+                .output_len(32)
+                .build()
+            {
+                Ok(params) => params,
+                Err(e) => return Err(LuaError::external(format!("Failed to create Argon2 parameters: {}", e))),
+            },
+        );
+
+        let mut hashed_key = vec![0u8; 32];
+        argon2
+            .hash_password_into(key.as_bytes(), salt, &mut hashed_key)
+            .map_err(|e| LuaError::external(format!("Failed to hash password: {e:?}")))?;
+
+        let cipher = Aes256Gcm::new_from_slice(&hashed_key)
+        .map_err(LuaError::external)?;
+
+        Ok(cipher)
     }
 }
 
@@ -60,7 +97,66 @@ impl LuaUserData for Blob {
 
         methods.add_method("clone", |_, this, ()| {
             Ok(Blob { data: this.data.clone() })
-        })
+        });
+
+        methods.add_method_mut("drain", |_, this, ()| {
+            std::mem::take(&mut this.data);
+            Ok(())
+        });
+
+        methods.add_method_mut("zeroize", |_, this, ()| {
+            this.data.zeroize();
+            std::mem::take(&mut this.data);
+            Ok(())
+        });
+
+        // AES-256 encryption of the blob
+        methods.add_method("aes256encrypt", |_, this, key: String| {
+            let mut salt = [0u8; 8];
+            rand::rng().fill_bytes(&mut salt);
+
+            let cipher = Blob::create_aes256_cipher(key, &salt)?;
+
+            let random_slice = rand::rng().random::<[u8; 12]>();
+            let nonce = Nonce::from_slice(&random_slice);
+
+            let mut encrypted = cipher
+                .encrypt(nonce, &*this.data)
+                .map_err(|e| LuaError::external(format!("Failed to encrypt: {:?}", e)))?;
+
+            // Format must be <salt><nonce><ciphertext>
+            let mut result = Vec::with_capacity(8 + 12 + encrypted.len());
+            result.extend_from_slice(&salt);
+            result.extend_from_slice(nonce.as_slice());
+            result.append(&mut encrypted);
+
+            Ok(Blob {
+                data: result,
+            })
+        });
+
+        // AES-256 decryption of the blob
+        methods.add_method("aes256decrypt", |_, this, key: String| {
+            if this.data.len() < 20 {
+                return Err(LuaError::external("Blob data is too short to decrypt".to_string()));
+            }
+
+            let salt = &this.data[..8];
+            let nonce = &this.data[8..20];
+            let ciphertext = &this.data[20..]; 
+
+            let cipher = Blob::create_aes256_cipher(key, salt)?;
+
+            let nonce = Nonce::from_slice(nonce);
+
+            let result = cipher
+                .decrypt(nonce, ciphertext)
+                .map_err(|e| LuaError::external(format!("Failed to decrypt: {:?}", e)))?;
+
+            Ok(Blob {
+                data: result,
+            })
+        });
     }
 
     #[cfg(feature = "repl")]
