05e161ca8d11e88823e481d210084ae61825e9e0
Your Name <you@example.com>
2025-12-24 08:19:58 +0530

    drop dead code that was never removed

--- crates/runtime/src/plugins/antiraid/discord/structs.rs
+++ crates/runtime/src/plugins/antiraid/discord/structs.rs
@@ -166,11 +166,6 @@ impl MessagePagination {
 }
 
 #[derive(serde::Serialize, Default, serde::Deserialize)]
-pub struct GetAutoModerationRuleOptions {
-    pub rule_id: serenity::all::RuleId,
-}
-
-#[derive(serde::Serialize, Default, serde::Deserialize)]
 pub struct CreateAutoModerationRuleOptions {
     pub reason: String,
     pub data: CreateAutoModRule,
