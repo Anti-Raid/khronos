74b7666f3b2d8233801b8594acb580da9ebb8fb6
Your Name <you@example.com>
2026-01-28 10:33:46 -0500

    drop max delay requirement in favor of reinserting

--- crates/runtime/src/core/channel.rs
+++ crates/runtime/src/core/channel.rs
@@ -3,9 +3,10 @@ use mluau::prelude::*;
 use serenity::futures::{Stream, StreamExt};
 use serenity::futures::stream::FuturesUnordered;
 use tokio_util::time::DelayQueue;
+use std::cell::Cell;
 use std::task::{Context, Poll, Waker};
 use std::{cell::RefCell, pin::Pin};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::rc::{Rc, Weak};
 
 use crate::core::datetime::TimeDelta;
@@ -13,9 +14,7 @@ use crate::core::datetime::TimeDelta;
 const MAX_TIMEOUT: Duration = Duration::from_secs(7);
 
 const NUM_LEVELS: usize = 6;
-const MAX_DURATION: i64 = (1 << (6 * NUM_LEVELS)) - 1;
 const MAX_DURATION_UNSIGNED: u64 = (1 << (6 * NUM_LEVELS)) - 1;
-const MAX_DURATION_OBJ: TimeDelta = TimeDelta::from_millis(MAX_DURATION-5000);
 const MAX_DURATION_OBJ_STD: Duration = Duration::from_millis(MAX_DURATION_UNSIGNED-5000);
 
 pub struct OneshotChannel {
@@ -74,9 +73,18 @@ impl LuaUserData for OneshotChannel {
     }
 }
 
+// The key may change if the item is reinserted, so we use a Cell to allow mutability
+type SharedKey = Rc<Cell<Option<tokio_util::time::delay_queue::Key>>>;
+
+struct Item {
+    value: LuaValue,
+    final_expiry: std::time::Instant,
+    key: SharedKey
+}
+
 pub struct KeyHandle {
-    queue: Weak<RefCell<DelayQueue<LuaValue>>>,
-    key: tokio_util::time::delay_queue::Key,
+    queue: Weak<RefCell<DelayQueue<Item>>>,
+    key: SharedKey,
 }
 
 impl LuaUserData for KeyHandle {
@@ -86,10 +94,18 @@ impl LuaUserData for KeyHandle {
                 return Err(LuaError::external("Delay channel has been dropped"));
             };
 
+            let Some(key) = this.key.get() else {
+                return Ok((false, LuaValue::Nil)); // Already removed
+            };
+
+
             let val = match queue.try_borrow_mut()
             .map_err(LuaError::external)?
-            .try_remove(&this.key) {
-                Some(val) => Ok((true, val.into_inner())),
+            .try_remove(&key) {
+                Some(val) => {
+                    this.key.set(None); // Clear the key since it's been removed
+                    Ok((true, val.into_inner().value))
+                },
                 None => Ok((false, LuaValue::Nil)),
             };
 
@@ -104,7 +120,7 @@ impl LuaUserData for KeyHandle {
 
 /// A delay channel that can be used to return a value after a delay/expiration
 pub struct DelayChannel {
-    queue: Rc<RefCell<DelayQueue<LuaValue>>>,
+    queue: Rc<RefCell<DelayQueue<Item>>>,
     waiting_add: Rc<RefCell<Option<Waker>>>, // used to wake up the stream when a new item is added
 }
 
@@ -115,49 +131,37 @@ impl DelayChannel {
             waiting_add: Rc::new(RefCell::new(None)),
         }
     }
+
+    fn get_safe_delay(delay: Duration) -> Duration {
+        if delay > MAX_DURATION_OBJ_STD { MAX_DURATION_OBJ_STD } else { delay }
+    }
     
-    fn check_delay(delay: Duration) -> LuaResult<()> {
-        if delay > MAX_DURATION_OBJ_STD {
-            return Err(LuaError::external(format!("Duration is greater than max duration of {MAX_DURATION_OBJ_STD:?}")));
-        }
-        return Ok(())
-    }
-
-    /// Inserts a value into the delay channel with the given delay
-    pub fn add(&self, value: LuaValue, delay: Duration) -> LuaResult<()> {
-        Self::check_delay(delay)?;
-        self.queue.borrow_mut().insert(value, delay);
-        if let Some(waker) = self.waiting_add.borrow_mut().take() {
-            waker.wake();
-        }
-        Ok(())
-    }
-
     /// Inserts a value into the delay channel with the given delay
     /// and returns a handle that can be used to cancel it
-    pub fn add_with_handle(&self, value: LuaValue, delay: Duration) -> LuaResult<KeyHandle> {
-        Self::check_delay(delay)?;
-        let key = self.queue.borrow_mut().insert(value, delay);
+    pub fn add(&self, value: LuaValue, delay: Duration) -> LuaResult<KeyHandle> {
+        let final_expiry = Instant::now() + delay;
+        let safe_delay = Self::get_safe_delay(delay);
+        let key_cell = Rc::new(Cell::new(None));
+        let key = self.queue.borrow_mut().insert(Item { value, final_expiry, key: key_cell.clone() }, safe_delay);
+        key_cell.set(Some(key)); // Store the key in the cell for later retrieval
         if let Some(waker) = self.waiting_add.borrow_mut().take() {
             waker.wake();
         }
-        Ok(KeyHandle { key, queue: Rc::downgrade(&self.queue) })
+        Ok(KeyHandle { key: key_cell, queue: Rc::downgrade(&self.queue) })
     }
 
     pub async fn next(&self) -> LuaResult<LuaValue> {
-        loop {
-            let mut stream = QueueStream {
-                queue: self.queue.clone(),
-                waiting_add: self.waiting_add.clone(),
-            };
+        let mut stream = QueueStream {
+            queue: self.queue.clone(),
+            waiting_add: self.waiting_add.clone(),
+        };
 
-            // Attempt to get the next expired item
-            match StreamExt::next(&mut stream).await {
-                Some(expired) => return Ok(expired.into_inner()),
-                None => {
-                    // This should never happen, but just in case
-                    return Err(LuaError::external("Delay channel closed unexpectedly"));
-                }
+        // Attempt to get the next expired item
+        match StreamExt::next(&mut stream).await {
+            Some(value) => return Ok(value),
+            None => {
+                // This should never happen, but just in case
+                return Err(LuaError::external("Delay channel closed unexpectedly"));
             }
         }
     }
@@ -165,18 +169,9 @@ impl DelayChannel {
 
 impl LuaUserData for DelayChannel {
     fn add_methods<M: mluau::UserDataMethods<Self>>(methods: &mut M) {
-        methods.add_method("maxdelay", |_lua, _this, _: ()| {
-            Ok(MAX_DURATION_OBJ)
-        });
-
         methods.add_method("add", |_, this, (value, delay): (LuaValue, LuaUserDataRef<TimeDelta>)| {
             let delay = delay.timedelta.to_std().map_err(LuaError::external)?;
             this.add(value, delay)
-        });
-
-        methods.add_method("addwithhandle", |_, this, (value, delay): (LuaValue, LuaUserDataRef<TimeDelta>)| {
-            let delay = delay.timedelta.to_std().map_err(LuaError::external)?;
-            this.add_with_handle(value, delay)
         });
 
         methods.add_method("clear", |_, this, (): ()| {
@@ -194,23 +189,47 @@ impl LuaUserData for DelayChannel {
 }
 
 struct QueueStream {
-    queue: Rc<RefCell<DelayQueue<LuaValue>>>,
+    queue: Rc<RefCell<DelayQueue<Item>>>,
     waiting_add: Rc<RefCell<Option<Waker>>>,
 }
 
 impl Stream for QueueStream {
-    type Item = tokio_util::time::delay_queue::Expired<LuaValue>;
+    type Item = LuaValue;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut queue = self.queue.borrow_mut();
         
-        match queue.poll_expired(cx) {
-            Poll::Ready(Some(item)) => Poll::Ready(Some(item)),
-            Poll::Pending => Poll::Pending,
-            Poll::Ready(None) => { // We want to wait for new items to be added
-                // Store the waker so `add` can wake us up later
-                *self.waiting_add.borrow_mut() = Some(cx.waker().clone());
-                Poll::Pending
+        loop {
+            match queue.poll_expired(cx) {
+                Poll::Ready(Some(item)) => {
+                    let item = item.into_inner();
+                    let now = Instant::now();
+                    let key_cell = item.key.clone();
+
+                    if now < item.final_expiry {
+                        // If we woke up too early, reinsert with the remaining time
+                        // and keep looping
+                        //
+                        // The loop will hit poll_expired again, which registers the waker 
+                        // for this new item (or the next earliest item).
+                        let remaining = item.final_expiry - now;
+                        let safe_delay = DelayChannel::get_safe_delay(remaining);
+                        let new_key = queue.insert(item, safe_delay);
+                        key_cell.set(Some(new_key)); // Update the key in the cell
+                        continue;
+                    } else {
+                        // We've actually expired here, return the value
+                        key_cell.set(None); // Clear the key since it's been removed
+                        return Poll::Ready(Some(item.value));
+                    }
+                },
+                Poll::Pending => return Poll::Pending,
+                Poll::Ready(None) => { 
+                    // We want to wait for new items to be added
+                    // Store the waker so `add` can wake us up later
+                    *self.waiting_add.borrow_mut() = Some(cx.waker().clone());
+                    return Poll::Pending
+                }
             }
         }
     }
