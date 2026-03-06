57b0b33ce2758307723eaa51ad51fc517632765b
Your Name <you@example.com>
2026-03-06 18:06:41 +0200

    Add dedicaed vfs type to khronosvalue

--- crates/runtime/src/traits/ir/globalkv.rs
+++ crates/runtime/src/traits/ir/globalkv.rs
@@ -1,6 +1,5 @@
 use mluau::prelude::*;
-use serde::{Deserialize, Serialize};
-use crate::{core::datetime::DateTime, primitives::{lazy::Lazy, opaque::Opaque}};
+use crate::{core::datetime::DateTime, primitives::opaque::Opaque, utils::khronos_value::KhronosValue};
 
 /// A global key-value entry that can be viewed by all guilds
 /// 
@@ -18,7 +17,7 @@ pub struct PartialGlobalKv {
     pub owner_type: String,
     pub price: Option<i64>, // will only be set for shop items, otherwise None
     pub short: String, // short description for the key-value.
-    pub public_metadata: serde_json::Value, // public metadata about the key-value
+    pub public_metadata: KhronosValue, // public metadata about the key-value
     pub scope: String,
     pub created_at: chrono::DateTime<chrono::Utc>,
     pub last_updated_at: chrono::DateTime<chrono::Utc>,
@@ -43,7 +42,7 @@ impl IntoLua for PartialGlobalKv {
             Some(l) => LuaValue::String(lua.create_string(&l)?),
             None => LuaValue::Nil,
         })?;
-        table.set("public_metadata", lua.to_value(&self.public_metadata)?)?;
+        table.set("public_metadata", self.public_metadata)?;
         table.set("scope", self.scope)?;
         table.set("created_at", DateTime::from_utc(self.created_at))?;
         table.set("last_updated_at", DateTime::from_utc(self.last_updated_at))?;
@@ -70,7 +69,7 @@ pub struct GlobalKv {
 
 pub enum GlobalKvData {
     Value {
-        data: serde_json::Value,
+        data: KhronosValue,
         opaque: bool,
     },
     PurchaseRequired {
@@ -85,13 +84,12 @@ impl IntoLua for GlobalKv {
         let data_table = lua.create_table()?;
         match self.data {
             GlobalKvData::Value { data, opaque } => {
-                let ud = if opaque {
-                    lua.create_userdata(Opaque::new(data))?
+                if opaque {
+                    data_table.set("data", Opaque::new(data))?;
                 } else {
-                    lua.create_userdata(Lazy::new(data))?
+                    data_table.set("data", data)?;
                 };
                 data_table.set("type", "Value")?;
-                data_table.set("data", ud)?;
             }
             GlobalKvData::PurchaseRequired { purchase_url } => {
                 data_table.set("type", "PurchaseRequired")?;
@@ -120,14 +118,41 @@ impl IntoLua for GlobalKv {
 /// Only staff may create priced global KV's.
 /// NOTE 2: All Global KV's undergo staff review before being made available. When this occurs,
 /// review state will be updated accordingly from 'pending' to 'approved' or otherwise if rejected.
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct CreateGlobalKv {
     pub key: String,
     pub version: i32,
     pub short: String, // short description for the key-value.
-    pub public_metadata: serde_json::Value, // public metadata about the key-value
+    pub public_metadata: KhronosValue, // public metadata about the key-value
     pub scope: String,
     pub public_data: bool,
     pub long: Option<String>, // long description for the key-value.
-    pub data: serde_json::Value, // the actual value of the key-value, may be private
+    pub data: KhronosValue, // the actual value of the key-value, may be private
+}
+
+impl FromLua for CreateGlobalKv {
+    fn from_lua(lua_value: LuaValue, _lua: &Lua) -> LuaResult<Self> {
+        let table = match lua_value {
+            LuaValue::Table(t) => t,
+            _ => return Err(LuaError::FromLuaConversionError { from: lua_value.type_name(), to: "CreateGlobalKv".to_string(), message: Some("Expected a table".to_string()) }),
+        };
+        let key = table.get("key")?;
+        let version = table.get("version")?;
+        let short = table.get("short")?;
+        let public_metadata = table.get("public_metadata")?;
+        let scope = table.get("scope")?;
+        let public_data = table.get("public_data")?;
+        let long: Option<String> = table.get("long")?;
+        let data = table.get("data")?;
+        Ok(Self {
+            key,
+            version,
+            short,
+            public_metadata,
+            scope,
+            public_data,
+            long,
+            data,
+        })
+    }
 }
