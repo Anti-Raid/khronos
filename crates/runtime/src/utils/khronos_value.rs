da75b64e7d146b2bf9469a289a56b45fa3fb5723
Your Name <you@example.com>
2026-06-15 20:23:18 +0530

    add CKhronosValue

--- crates/runtime/src/utils/khronos_value.rs
+++ crates/runtime/src/utils/khronos_value.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
 
 use mluau::prelude::*;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, ser::{SerializeMap, SerializeSeq}};
 
 use crate::core::typesext::MemoryVfs;
 
@@ -53,12 +53,13 @@ pub enum KhronosValue {
     Interval(chrono::Duration),
     TimeZone(chrono_tz::Tz),
     MemoryVfs(Box<HashMap<String, String>>),
-    Null,
+    Nil(()),
+    Null(()),
 }
 
 impl Default for KhronosValue {
     fn default() -> Self {
-        KhronosValue::Null
+        KhronosValue::Nil(())
     }
 }
 
@@ -73,6 +74,10 @@ impl KhronosValue {
             });
         }
 
+        if value.is_null() {
+            return Ok(KhronosValue::Null(()))
+        }
+
         match value {
             LuaValue::String(s) => Ok(KhronosValue::Text(s.to_string_lossy())),
             LuaValue::Integer(i) => Ok(KhronosValue::Integer(i)),
@@ -84,7 +89,7 @@ impl KhronosValue {
                 Err(LuaError::FromLuaConversionError { from: "buffer", to: "KhronosValue".to_string(), message: Some("Cannot send/store buffers.".to_string()) })
             }
             LuaValue::Vector(v) => Ok(KhronosValue::Vector((v.x(), v.y(), v.z()))),
-            LuaValue::Nil => Ok(KhronosValue::Null),
+            LuaValue::Nil => Ok(KhronosValue::Null(())),
             LuaValue::Table(table) => {
                 if table.raw_len() == 0 {
                     // Check for array metatable
@@ -208,7 +213,8 @@ impl KhronosValue {
             KhronosValue::Interval(i) => crate::core::datetime::TimeDelta::new(i).into_lua(lua),
             KhronosValue::TimeZone(tz) => crate::core::datetime::Timezone::new(tz).into_lua(lua),
             KhronosValue::MemoryVfs(m) => MemoryVfs::new(*m).into_lua(lua),
-            KhronosValue::Null => Ok(LuaValue::Nil),
+            KhronosValue::Nil(_) => Ok(LuaValue::Nil),
+            KhronosValue::Null(_) => Ok(LuaValue::NULL),
         }
     }
 }
@@ -222,5 +228,217 @@ impl FromLua for KhronosValue {
 impl IntoLua for KhronosValue {
     fn into_lua(self, lua: &Lua) -> LuaResult<LuaValue> {
         KhronosValue::into_lua_impl(self, lua, 0)
+    }
+}
+
+/// A small 'compressed' representation of a KhronosValue (at the cost of non-self-describability)
+/// 
+/// Only supports JSON/MessagePack
+#[derive(Debug, Clone)]
+pub struct CKhronosValue(pub KhronosValue);
+
+/// Similar to CKhronosValue but takes a ref (internally, CKhronosValue just uses this for serializing)
+pub struct CKhronosValueRef<'a>(&'a KhronosValue);
+impl<'a> Serialize for CKhronosValueRef<'a> {    
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+    S: serde::Serializer {
+        struct CompressedStrMap<'a>(&'a std::collections::HashMap<String, KhronosValue>);
+
+        impl<'a> Serialize for CompressedStrMap<'a> {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                let mut map = serializer.serialize_map(Some(self.0.len()))?;
+                for (k, v) in self.0.iter() {
+                    map.serialize_entry(k, &CKhronosValueRef(v))?;
+                }
+                map.end()
+            }
+        }
+
+        struct CompressedMap<'a>(&'a [(KhronosValue, KhronosValue)]);
+
+        impl<'a> Serialize for CompressedMap<'a> {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                let mut map = serializer.serialize_seq(Some(self.0.len()))?;
+                for (k, v) in self.0.iter() {
+                    map.serialize_element(&(CKhronosValueRef(k), CKhronosValueRef(v)))?;
+                }
+                map.end()
+            }
+        }
+
+        pub struct IntStr(i64);
+        impl Serialize for IntStr {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                string_i64::serialize(&self.0, serializer)
+            }
+        }
+
+        match self.0 {
+            KhronosValue::Text(st) => serializer.serialize_str(st),
+            KhronosValue::Nil(_) => serializer.serialize_unit(),
+            KhronosValue::Boolean(b) => serializer.serialize_bool(*b),
+            KhronosValue::List(l) => {
+                let mut seq = serializer.serialize_seq(Some(l.len()))?;
+                for v in l {
+                    seq.serialize_element(&CKhronosValueRef(v))?; 
+                }
+                seq.end()
+            }
+            KhronosValue::Integer(i) => serializer.serialize_newtype_variant("Compressed", 0, "I", i),
+            KhronosValue::Float(i) => serializer.serialize_newtype_variant("Compressed", 1, "F", i),
+            KhronosValue::Int64(i) => serializer.serialize_newtype_variant("Compressed", 2, "I64", &IntStr(*i)),
+            KhronosValue::Map(m) => serializer.serialize_newtype_variant("Compressed", 3, "M", &CompressedMap(m.as_ref())),
+            KhronosValue::StrMap(m) => serializer.serialize_newtype_variant("Compressed", 4, "#SM", &CompressedStrMap(m.as_ref())),
+            KhronosValue::Vector(m) => serializer.serialize_newtype_variant("Compressed", 5, "Vec", m),
+            KhronosValue::Timestamptz(m) => serializer.serialize_newtype_variant("Compressed", 6, "TS", m),
+            KhronosValue::Interval(m) => serializer.serialize_newtype_variant("Compressed", 7, "Interval", m),
+            KhronosValue::TimeZone(m) => serializer.serialize_newtype_variant("Compressed", 8, "TZ", m),
+            KhronosValue::MemoryVfs(m) => serializer.serialize_newtype_variant("Compressed", 9, "MVfs", m),
+            KhronosValue::Null(m) => serializer.serialize_newtype_variant("Compressed", 10, "N", m),
+        }    
+    }
+}
+
+struct CKhronosVisitor;
+
+impl<'de> serde::de::Visitor<'de> for CKhronosVisitor {
+    type Value = CKhronosValue;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("a valid CKhronosValue payload")
+    }
+
+    fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(CKhronosValue(KhronosValue::Boolean(v)))
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(CKhronosValue(KhronosValue::Text(v.to_owned())))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(CKhronosValue(KhronosValue::Text(v)))
+    }
+
+    fn visit_unit<E>(self) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(CKhronosValue(KhronosValue::Nil(())))
+    }
+
+    fn visit_none<E>(self) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(CKhronosValue(KhronosValue::Null(())))
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let mut list = Vec::with_capacity(seq.size_hint().unwrap_or(0));
+        
+        while let Some(val) = seq.next_element::<CKhronosValue>()? {
+            list.push(val.0);
+        }
+        
+        Ok(CKhronosValue(KhronosValue::List(list)))
+    }
+
+    fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+    where
+        M: serde::de::MapAccess<'de>,
+    {
+        let key: String = map.next_key()?
+            .ok_or_else(|| serde::de::Error::custom("expected a tag key for non-primitive CKhronosValue but found an empty object"))?;
+
+        let value = match key.as_str() {
+            "I" => KhronosValue::Integer(map.next_value()?),
+            "F" => KhronosValue::Float(map.next_value()?),
+            "I64" => {
+                let s: String = map.next_value()?;
+                KhronosValue::Int64(s.parse::<i64>().map_err(serde::de::Error::custom)?)
+            }
+            "M" => {
+                let pairs: Vec<(CKhronosValue, CKhronosValue)> = map.next_value()?;
+                // Strip the wrappers off the children
+                let uncompressed = pairs.into_iter().map(|(k, v)| (k.0, v.0)).collect();
+                KhronosValue::Map(uncompressed)
+            }
+            "#SM" => {
+                let strmap: HashMap<String, CKhronosValue> = map.next_value()?;
+                // Strip the wrappers off the children
+                let uncompressed = strmap.into_iter().map(|(k, v)| (k, v.0)).collect();
+                KhronosValue::StrMap(Box::new(uncompressed))
+            }
+            "Vec" => KhronosValue::Vector(map.next_value()?),
+            "TS" => KhronosValue::Timestamptz(map.next_value()?),
+            "Interval" => KhronosValue::Interval(map.next_value()?),
+            "TZ" => KhronosValue::TimeZone(map.next_value()?),
+            "MVfs" => KhronosValue::MemoryVfs(map.next_value()?),
+            "N" => KhronosValue::Null(map.next_value()?),
+            _ => return Err(serde::de::Error::custom(format!("Unknown tag: {}", key))),
+        };
+
+        Ok(CKhronosValue(value))
+    }
+}
+
+impl Serialize for CKhronosValue {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+    S: serde::Serializer {
+        CKhronosValueRef(&self.0).serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for CKhronosValue {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_any(CKhronosVisitor)
+    }
+}
+
+#[cfg(test)]
+mod test_compressed {
+    use std::collections::HashMap;
+
+    use crate::utils::khronos_value::{CKhronosValue, CKhronosValueRef, KhronosValue};
+
+    #[test]
+    fn test_base() {
+        let mut my_map = HashMap::new();
+        my_map.insert("foo".to_string(), KhronosValue::Boolean(true));
+        my_map.insert("carrot".to_string(), KhronosValue::Integer(23));
+        my_map.insert("bar".to_string(), KhronosValue::Int64(284));
+        my_map.insert("baz".to_string(), KhronosValue::List(vec![KhronosValue::Int64(333), KhronosValue::Nil(()), KhronosValue::Nil(()), KhronosValue::Null(()), KhronosValue::Text("Hello?".to_string())]));
+        let my_kv = KhronosValue::StrMap(Box::new(my_map));
+        let s = serde_json::to_string_pretty(&CKhronosValueRef(&my_kv)).expect("failed to serde");
+        println!("{}", s);
+        let deser = serde_json::from_str::<CKhronosValue>(&s).expect("failed to deser");
+        println!("{:?}", deser)
     }
 }
